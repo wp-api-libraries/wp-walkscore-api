0b489eafed3545214d4d6ee23d72211845b52d5c
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-10-26 13:19:44 -0700

    Update wp-walkscore-api.php

--- wp-walkscore-api.php
+++ wp-walkscore-api.php
@@ -5,10 +5,21 @@
  * @package WP-Walkscore-API
  */
 
-/* Exit if accessed directly */
+/*
+* Plugin Name: WP WalkScore API
+* Plugin URI: https://github.com/wp-api-libraries/wp-walkscore-api
+* Description: Perform API requests to Walkscore in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-walkscore-api
+* GitHub Branch: master
+*/
+
+/* Exit if accessed directly. */
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-
+/* Check if class exists. */
 if ( ! class_exists( 'WalkscoreAPI' ) ) {
 
 	/**
